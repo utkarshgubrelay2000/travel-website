4ab67fb732bbb68ee24cdd78e037ef1b568b59ee
utkarshgubrelay <utkarshgubrelay2000@gmail.com>
2021-03-16 15:12:51 +0530

    userId

--- model/adModel.js
+++ model/adModel.js
@@ -12,10 +12,11 @@ const Schema= new mongoose.Schema({
          includes:[],
          keywords:[],
          generalInfo:{type:String},
+         tripType:[],
          testimonial:[
              {
-            reviewBy:{type:String},
-            rating:{type:Number,enum:[0,1,2,3,4,5,6,7,8,9]},
+            reviewBy:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+            rating:{type:Number,enum:[0,1,2,3,4,5,6,7,8,9],default:0},
             reviewContent:{type:String},
             replies:[{_id:false,reply:{type:String},replyBy:{type:String}}]
             }
