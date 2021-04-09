7cd0d81140e6471b2c3b7c0dce36456f9e545073
utkarshgubrelay <utkarshgubrelay2000@gmail.com>
2021-04-09 16:11:43 +0530

    bug

--- controller/imageController.js
+++ controller/imageController.js
@@ -15,8 +15,12 @@ exports.postImages=(req,res)=>{
     Images.find({}).then(found=>{
         try {
             Images.findById(found[0]._id).then(foundImage=>{
-                let image={url:req.body.url,rating:req.body.rating,tourPlace:req.body.tourPlace}
-                foundImage.Images.push(image)
+                let image={url:req.body.url,
+                    rating:req.body.rating,tourPlace:req.body.tourPlace}
+                let array=foundImage.Images
+                array.push(image)
+                foundImage.Images=array
+                console.log(foundImage,req.body.url)
                 foundImage.save()
 res.json('done')
             })
